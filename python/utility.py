14f6e3f131fe808a5b29f3a2a812388336c45dc8
KM <35831522+km-git-acc@users.noreply.github.com>
2018-01-28 17:27:22 +0530

    Making Ht_real safe against complex inputs

--- python/utility.py
+++ python/utility.py
@@ -32,13 +32,6 @@ def phi_decay(u,n_max=100):
 def Ht_complex_integrand(u, z, t):
      return exp(t*u*u)*phi_decay(u)*cos(z*u)
 
-def Ht_real_integrand(u, z, t):
-     return exp(t*u*u)*phi_decay(u)*cos(z*u)
-
-def Ht_real(z,t):
-     #return quad(Ht_real_integrand, 0, np.inf, args=(z,t)) #causing overflow errors so np.inf replaced with 10
-     return quad(Ht_real_integrand, 0, 10, args=(z,t))
-
 def Ht_complex(z,t):
     #may work well only for small to medium values of z  
     def real_func(a,b,c): return scipy.real(Ht_complex_integrand(a,b,c))
@@ -47,6 +40,20 @@ def Ht_complex(z,t):
     imag_part = quad(imag_func, 0, 10, args=(z,t))
     return (real_part[0] + 1j*imag_part[0], real_part[1], imag_part[1])
 
+def Ht_real_integrand(u, z, t):
+     if(abs(scipy.imag(z))>0 or abs(scipy.imag(t))>0 or abs(scipy.imag(u))>0): 
+        print ("complex values not allowed for this function")
+        return "error"
+     u,z,t = scipy.real(u),scipy.real(z),scipy.real(t)
+     return scipy.real(exp(t*u*u)*phi_decay(u)*cos(z*u))
+
+def Ht_real(z,t):
+     if(abs(scipy.imag(z))>0 or abs(scipy.imag(t))>0): 
+        print ("complex values not allowed for this function")
+        return "error"
+     z,t = scipy.real(z),scipy.real(t)
+     #return quad(Ht_real_integrand, 0, np.inf, args=(z,t)) #causing overflow errors so np.inf replaced with 10
+     return quad(Ht_real_integrand, 0, 10, args=(z,t))
 
 #check phi_decay values
 '''print (phi_decay(0.001))
