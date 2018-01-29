4455e2af47df1752e4326fa22b8b37ebd87eb761
KM <35831522+km-git-acc@users.noreply.github.com>
2018-01-29 07:20:45 +0530

    No functions to be loaded from Ht_real_compute
    Ht_real and Ht_real integrand are now available from utility.py

--- dbn_upper_bound/python/Ht_real_compute.py
+++ dbn_upper_bound/python/Ht_real_compute.py
@@ -1,3 +1,4 @@
+'''
 from scipy.integrate import quad
 import numpy as np
 import math
@@ -10,6 +11,13 @@ def Ht_real_integrand(u, z, t):
 def Ht_real(z,t):
      #return quad(Ht_real_integrand, 0, np.inf, args=(z,t)) #causing overflow errors so np.inf replaced with 10
      return quad(Ht_real_integrand, 0, 10, args=(z,t))
+'''
+'''
+The above part is no longer necessary since the functions are present in utility.py. 
+It could be that the math functions are faster than the cmath functions in which case 
+we may have to make further changes to speed up Ht_real computations'''
+
+'''The snippet below are simple loops for evaluating Ht_real on multiple points. This can be reused if required'''
 
 #check values of Ht_real
 #for t in range(0,6):
