65dadf1f7544ade797bacf88c4ebdb7f62a76ee8
Sujit Nair <sujit.nair@gmail.com>
2018-01-28 16:36:35 -0800

    Fix pylint complaints for Ht_root_finder.py

--- dbn_upper_bound/python/Ht_root_finder.py
+++ dbn_upper_bound/python/Ht_root_finder.py
@@ -1,8 +1,8 @@
-import math
-from math import *
-from utility import *
-from Ht_real_compute import *
-from scipy.optimize import *
+"""
+TODO: Add details on what this module does
+"""
+from scipy.optimize import fsolve
+from Ht_real_compute import Ht_real
 
 H0_z1 = 2*14.1347251417346937904572519835624702707842
 H0_z2 = 2*21.022039638771554992628479593896902777
@@ -12,10 +12,10 @@ H0_z5 = 2*32.9350615877391896906623689640749034888127156
 H0_z6 = 2*37.586178158825671257217763480705332821405597
 
 #give a nearby H0 zero, not necessarily from the above
-nearby_root = H0_z5 
+nearby_root = H0_z5
 
-for i in range(0,501):
-      t=float(i)/1000.0
-      Ht_root = fsolve(Ht_real,nearby_root,args=(t,))[0]
-      print (t,Ht_root)
-      nearby_root=Ht_root
+for i in range(501):
+    t = float(i)/1000.0
+    Ht_root = fsolve(Ht_real, nearby_root, args=(t,))[0]
+    print(t, Ht_root)
+    nearby_root = Ht_root
