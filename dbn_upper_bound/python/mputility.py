df1b1cd642abe9820b184d6884cc74dcb52c1226
KM <35831522+km-git-acc@users.noreply.github.com>
2018-02-13 22:31:22 +0530

    Added miscellaneous functions to mputility

--- dbn_upper_bound/python/mputility.py
+++ dbn_upper_bound/python/mputility.py
@@ -6,7 +6,6 @@ first few functions, please check utility.py. The new ones are explained below.
 Default precision is set to 30 significant digits below, but
 this can be overriden in your code. If you can get the gmpy2 library
 installed, that could provide a major speed boost for mpmath
-
 The new functions (not present in utility.py) are
 1) The main and helper functions for computing the Riemann Siegel
     Z function used to compute zeta zeroes (for benchmarking purposes),
@@ -23,12 +22,12 @@ mp.dps = 30
 
 def phi_decay(u, n_max=100):
     """
-    Computes \Phi(u) in Terry' blog at
+    Computes Phi(u) in Terry' blog at
     https://terrytao.wordpress.com/2018/02/02/polymath15-second-thread-generalising-the-riemann-siegel-approximate-functional-equation/
     :param u: input complex number
     :param n_max: upper limit for summation. It has to be a positive
     integer
-    :return: \Phi(u)
+    :return: Phi(u)
     """
     running_sum = 0
     u = mp.mpc(u)
@@ -51,16 +50,16 @@ def Ht_complex_integrand(u, z, t):
     :return: integrand of H_t(z)
     """
     u, z, t = mp.mpc(u), mp.mpc(z), mp.mpc(t)
-    return mp.exp(t*u*u) * phi_decay(u) * mp.cos(z*u)
+    return mp.exp(t * u * u) * phi_decay(u) * mp.cos(z * u)
 
 
-def Ht_complex(z,t):
+def Ht_complex(z, t):
     #may work well only for small to medium values of z  
-    part = mp.quad(lambda u: Ht_complex_integrand(u,z,t), [0, 10])
+    part = mp.quad(lambda u: Ht_complex_integrand(u, z, t), [0, 10])
     return part
 
-def Ht_complex_root_finder(complex_guess,t):
-    result = mp.findroot(lambda z: Ht_complex(z,t),mp.mpc(complex_guess))
+def Ht_complex_root_finder(complex_guess, t):
+    result = mp.findroot(lambda z: Ht_complex(z, t), mp.mpc(complex_guess))
     return result
 
 '''Begin Riemann Siegel Z block'''
@@ -69,41 +68,40 @@ def RStheta(x):
     return (x/2)*(mp.log(x/(2*mp.pi())) - 1) - mp.pi()/8 + 1/(48*x) + 7/(5760*mp.power(x,3)) 
 
 def c0(p):
-   return mp.cos(2*mp.pi()*(p*p - p - 1/16))/mp.cos(2*mp.pi()*p)
+    return mp.cos(2*mp.pi()*(p*p - p - 1/16))/mp.cos(2*mp.pi()*p)
 
 def c1(p):
-   return (-1/(96*mp.pi()*mp.pi()))*mp.diff(lambda x: c0(x), p, 3)
+    return (-1/(96*mp.pi()*mp.pi()))*mp.diff(lambda x: c0(x), p, 3)
 
 def RSZ_plain(x):
     x = mp.mpf(x.real)
     tau = mp.sqrt(x/(2*mp.pi()))
     N = int(tau.real)
     z = 2*(x-N)-1
-    running_sum=0
-    for n in range(1,N+1):
+    running_sum = 0
+    for n in range(1, N+1):
         running_sum += mp.cos(RStheta(x) - x*mp.log(n))/mp.sqrt(n)
-    return (2*running_sum).real
+    return (2 * running_sum).real
 
 def RSZ_upto_c0(x):
     x = mp.mpf(x.real)
-    tau = mp.sqrt(x/(2*mp.pi()))
+    tau = mp.sqrt(x / (2 * mp.pi()))
     N = int(tau.real)
-    p = tau-N
-    running_sum=0
-    for n in range(1,N+1):
-        running_sum += mp.cos(RStheta(x) - x*mp.log(n))/mp.sqrt(n)
-    return (2*running_sum + mp.power(-1,N-1)*mp.power(tau,-0.5)*c0(p)).real
+    p = tau - N
+    running_sum = 0
+    for n in range(1, N+1):
+        running_sum += mp.cos(RStheta(x) - x * mp.log(n)) / mp.sqrt(n)
+    return (2 * running_sum + mp.power(-1,N-1) * mp.power(tau,-0.5) * c0(p)).real
 
 def RSZ_upto_c1(x):
     x = mp.mpf(x.real)
-    tau = mp.sqrt(x/(2*mp.pi()))
+    tau = mp.sqrt(x / (2 * mp.pi()))
     N = int(tau.real)
-    p = tau-N
-    running_sum=0
-    for n in range(1,N+1):
-        #print (n,N)
-        running_sum += mp.cos(RStheta(x) - x*mp.log(n))/mp.sqrt(n)
-    return (2*running_sum + mp.power(-1,N-1)*mp.power(tau,-0.5)*(c0(p) - (1/tau)*c1(p))).real
+    p = tau - N
+    running_sum = 0
+    for n in range(1, N+1):
+        running_sum += mp.cos(RStheta(x) - x * mp.log(n)) / mp.sqrt(n)
+    return (2 * running_sum + mp.power(-1,N-1) * mp.power(tau,-0.5) * (c0(p) - (1/tau) * c1(p))).real
 
 '''End Riemann Siegel Z block'''
 
@@ -112,49 +110,49 @@ def RSZ_upto_c1(x):
 def F0(s):
     # works only for x > 14 approx due to the sqrt(x/4PI) factor
     term1 = mp.power(mp.pi(),-1*s/2)*mp.gamma((s+4)/2)
-    x=2*s.imag
+    x = 2 * s.imag
     N = int(mp.sqrt(x/(4*mp.pi())))
-    running_sum=0
-    for n in range(1,N+1):
+    running_sum = 0
+    for n in range(1, N+1):
         running_sum += 1/mp.power(n,s)
-    return term1*running_sum
+    return term1 * running_sum
 
 def H0_AFE(z):
     z = mp.mpc(z)
     f_arg1 = (1 + 1j*z.real - z.imag)/2
     f_arg2 = (1 + 1j*z.real + z.imag)/2
-    if z.imag==0:
-        result= 0.5*(F0(f_arg1).real)
+    if z.imag == 0:
+        result = 0.5 * (F0(f_arg1).real)
     else: 
-        result = 0.25*(F0(f_arg1) + F0(f_arg2).conjugate())
+        result = 0.25 * (F0(f_arg1) + F0(f_arg2).conjugate())
     return result
 
-def Ft(s,t):
+def Ft(s, t):
     # works only for x > 14 approx due to the sqrt(x/4PI) factor
-    term1 = mp.power(mp.pi(),-1*s/2)*mp.gamma((s+4)/2)
-    x=2*s.imag
-    N = int(mp.sqrt(x/(4*mp.pi())))
-    running_sum=0
-    for n in range(1,N+1):
-        running_sum += mp.exp((t/16)*mp.power(mp.log((s+4)/(2*mp.pi()*n*n)),2))/mp.power(n,s) # main eqn 
-    return term1*running_sum
+    term1 = mp.power(mp.pi(),-1*s/2) * mp.gamma((s+4)/2)
+    x = 2 * s.imag
+    N = int(mp.sqrt(x/(4 * mp.pi())))
+    running_sum = 0
+    for n in range(1, N+1):
+        running_sum += mp.exp((t/16) * mp.power(mp.log((s+4) / (2 * mp.pi() * n * n)),2)) / mp.power(n,s) # main eqn 
+    return term1 * running_sum
 
-def Ht_AFE(z,t):
-    z,t = mp.mpc(z),mp.mpc(t)
+def Ht_AFE(z, t):
+    z, t = mp.mpc(z), mp.mpc(t)
     f_arg1 = (1 + 1j*z.real - z.imag)/2
     f_arg2 = (1 + 1j*z.real + z.imag)/2
-    if z.imag==0:
-        result= 0.5*(Ft(f_arg1,t).real)
+    if z.imag == 0:
+        result = 0.5 * (Ft(f_arg1,t).real)
     else: 
-        result = 0.25*(Ft(f_arg1,t) + Ft(f_arg2,t).conjugate())
+        result = 0.25 * (Ft(f_arg1,t) + Ft(f_arg2,t).conjugate())
     return result
 
 def psi(p):
-    term1 = 2*mp.pi()
-    term2 = mp.cos(mp.pi()*(p*p/2 - p - 1/8))
-    term3 = mp.exp(0.5j*mp.pi()*p*p - 5j*mp.pi()/8)
-    term4 = mp.cos(mp.pi()*p)
-    return term1*term2*term3/term4
+    term1 = 2 * mp.pi()
+    term2 = mp.cos(mp.pi() * (p*p/2 - p - 1/8))
+    term3 = mp.exp(0.5j * mp.pi() * p * p - 5j * mp.pi()/8)
+    term4 = mp.cos(mp.pi() * p)
+    return term1 * term2 * term3 / term4
 
 
 def Ht_AFE_A(z, t):
@@ -174,7 +172,7 @@ def Ht_AFE_A(z, t):
             * mp.power(mp.pi(), -1*s/2) * mp.gamma(s/2)
     A_sum = 0.0
     for n in range(1, N+1):
-        if t.real>0:
+        if t.real > 0:
             A_sum += mp.exp((t/16) * mp.power(mp.log((s+4)/(2*mp.pi()*n*n)), 2))/mp.power(n, s)
         else:
             A_sum += 1/mp.power(n, s)
@@ -198,7 +196,7 @@ def Ht_AFE_B(z, t):
     B_pre = (1/16) * s * (s-1) * mp.power(mp.pi(), (s-1)/2) * mp.gamma((1-s)/2)
     B_sum = 0.0
     for m in range(1, M+1):
-        if t.real>0:
+        if t.real > 0:
             B_sum += mp.exp((t/16) * mp.power(mp.log((5-s)/(2 * mp.pi() * m * m)), 2))/mp.power(m, 1-s)
         else:
             B_sum += 1/mp.power(m, 1-s)
@@ -249,24 +247,23 @@ def Ht_AFE_ABC(z, t):
 '''End H_t approx functional eqn (Ht_AFE) block'''
 
 '''Miscellanous useful functions'''
-def Nt(t,T):
-    t,T=mp.mpf(t),mp.mpf(T)
-    Tsmall = T/(4*mp.pi())
-    N0 = Tsmall*mp.log(Tsmall) - Tsmall
-    extra = (t/16.0)*mp.log(Tsmall)
+def Nt(t, T):
+    t, T = mp.mpf(t), mp.mpf(T)
+    Tsmall = T/(4 * mp.pi())
+    N0 = Tsmall * mp.log(Tsmall) - Tsmall
+    extra = (t/16.0) * mp.log(Tsmall)
     Nt = N0 + extra
     return Nt.real
 
-def expected_zero_gap(t,T):
-    t,T=mp.mpf(t),mp.mpf(T)
-    return (T-0.9*T)/(Nt(t,T)-Nt(t,0.9*T))
+def expected_zero_gap(t, T):
+    t, T = mp.mpf(t), mp.mpf(T)
+    return (T - 0.9*T)/(Nt(t,T) - Nt(t,0.9*T))
 
-def append_data(filename,rows):
+def append_data(filename, rows):
     with open(filename, 'a') as csvfile:
-         writer = csv.writer(csvfile, delimiter=',',quotechar='"', quoting=csv.QUOTE_MINIMAL)
-         writer.writerows(rows)
+        writer = csv.writer(csvfile, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+        writer.writerows(rows)
 
 def sign_change(x, y):
     if x*y < 0: return 1
     else: return 0
-
