20b45f6d788151b29ad4d5932a7ebd6dbcc70468
dhjpolymath <35887467+dhjpolymath@users.noreply.github.com>
2018-02-13 21:21:25 +0000

    Run code without using venv by removing import section

--- dbn_upper_bound/python/utility.py
+++ dbn_upper_bound/python/utility.py
@@ -6,7 +6,7 @@ from cmath import exp, log, cos, sqrt
 from scipy.integrate import quad
 from scipy.optimize import fsolve
 import scipy
-from dbn_upper_bound.python.constants import PI, PI_sq
+from constants import PI, PI_sq
 
 
 def Nt(t, T):
