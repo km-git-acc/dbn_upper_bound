4ef83f0f6e9f89d62193239acb12f022ed504239
KM <35831522+km-git-acc@users.noreply.github.com>
2018-02-18 00:54:41 +0530

    Added faster version of A'+B' estimate

--- dbn_upper_bound/python/mputility.py
+++ dbn_upper_bound/python/mputility.py
@@ -36,13 +36,13 @@ def phi_decay(u, n_max=100):
                 - 3 * mp.pi() * mp.power(n, 2) * mp.exp(5*u)
         term2 = mp.exp(-1*mp.pi() * mp.power(n, 2) * mp.exp(4*u))
         running_sum += term1 * term2
-
+    
     return running_sum
 
 
 def Ht_complex_integrand(u, z, t):
     """
-    Computes the integrand of \H_t(u) in Terry' blog at
+    Computes the integrand of H_t(u) in Terry' blog at
     https://terrytao.wordpress.com/2018/02/02/polymath15-second-thread-generalising-the-riemann-siegel-approximate-functional-equation/
     :param u: integration parameter
     :param z: point at which H_t is computed
@@ -54,7 +54,7 @@ def Ht_complex_integrand(u, z, t):
 
 
 def Ht_complex(z, t):
-    #may work well only for small to medium values of z  
+    #may work well only for small to medium values of z
     part = mp.quad(lambda u: Ht_complex_integrand(u, z, t), [0, 10])
     return part
 
@@ -65,7 +65,7 @@ def Ht_complex_root_finder(complex_guess, t):
 '''Begin Riemann Siegel Z block'''
 
 def RStheta(x):
-    return (x/2)*(mp.log(x/(2*mp.pi())) - 1) - mp.pi()/8 + 1/(48*x) + 7/(5760*mp.power(x,3)) 
+    return (x/2)*(mp.log(x/(2*mp.pi())) - 1) - mp.pi()/8 + 1/(48*x) + 7/(5760*mp.power(x,3))
 
 def c0(p):
     return mp.cos(2*mp.pi()*(p*p - p - 1/16))/mp.cos(2*mp.pi()*p)
@@ -134,7 +134,7 @@ def Ft(s, t):
     N = int(mp.sqrt(x/(4 * mp.pi())))
     running_sum = 0
     for n in range(1, N+1):
-        running_sum += mp.exp((t/16) * mp.power(mp.log((s+4) / (2 * mp.pi() * n * n)),2)) / mp.power(n,s) # main eqn 
+        running_sum += mp.exp((t/16) * mp.power(mp.log((s+4) / (2 * mp.pi() * n * n)),2)) / mp.power(n,s) # main eqn
     return term1 * running_sum
 
 def Ht_AFE(z, t):
@@ -143,7 +143,7 @@ def Ht_AFE(z, t):
     f_arg2 = (1 + 1j*z.real + z.imag)/2
     if z.imag == 0:
         result = 0.5 * (Ft(f_arg1,t).real)
-    else: 
+    else:
         result = 0.25 * (Ft(f_arg1,t) + Ft(f_arg2,t).conjugate())
     return result
 
@@ -167,7 +167,7 @@ def Ht_AFE_A(z, t):
     s = (1 + 1j * z.real - z.imag)/2
     tau = mp.sqrt(s.imag/(2 * mp.pi()))
     N = int(tau)
-
+    
     A_pre = (1/16) * s * (s-1) \
             * mp.power(mp.pi(), -1*s/2) * mp.gamma(s/2)
     A_sum = 0.0
@@ -176,7 +176,7 @@ def Ht_AFE_A(z, t):
             A_sum += mp.exp((t/16) * mp.power(mp.log((s+4)/(2*mp.pi()*n*n)), 2))/mp.power(n, s)
         else:
             A_sum += 1/mp.power(n, s)
-
+    
     return A_pre * A_sum
 
 
@@ -192,7 +192,7 @@ def Ht_AFE_B(z, t):
     s = (1 + 1j * z.real - z.imag) / 2
     tau = mp.sqrt(s.imag/(2*mp.pi()))
     M = int(tau)
-
+    
     B_pre = (1/16) * s * (s-1) * mp.power(mp.pi(), (s-1)/2) * mp.gamma((1-s)/2)
     B_sum = 0.0
     for m in range(1, M+1):
@@ -200,7 +200,7 @@ def Ht_AFE_B(z, t):
             B_sum += mp.exp((t/16) * mp.power(mp.log((5-s)/(2 * mp.pi() * m * m)), 2))/mp.power(m, 1-s)
         else:
             B_sum += 1/mp.power(m, 1-s)
-
+    
     return B_pre * B_sum
 
 
@@ -217,12 +217,12 @@ def Ht_AFE_C(z, t):
     tau = mp.sqrt(s.imag/(2 * mp.pi()))
     N = int(tau)
     M = int(tau)
-
+    
     C_pre1 = -(1/16) * s * (s-1) * mp.power(mp.pi(), -1 * s/2) * mp.gamma(s/2)
     C_pre2 = mp.gamma(1-s) * mp.exp(-1j * mp.pi() * s)/(2j * mp.pi())
     C_pre3 = mp.power(2j * mp.pi() * M, s-1) * mp.exp(-1 * t * mp.pi() * mp.pi()/64)
     C_psi = psi((s/(2j * M * mp.pi())) - N)
-
+    
     return C_pre1 * C_pre2 * C_pre3 * C_psi
 
 
@@ -237,15 +237,15 @@ def Ht_AFE_ABC(z, t):
     A = Ht_AFE_A(z, t)
     B = Ht_AFE_B(z, t)
     C = Ht_AFE_C(z, t)
-
+    
     H = A+B+C
     if z.imag == 0:
         return H.real
     else:
         return H
 
-def AB_analysis(z,t):
-    z,t = mp.mpc(z),mp.mpc(t)
+def AB_analysis(z, t):
+    z, t = mp.mpc(z), mp.mpc(t)
     s = (1 + 1j*z.real - z.imag)/2
     tau = mp.sqrt(s.imag/(2*mp.pi()))
     N = int(tau)
@@ -253,19 +253,19 @@ def AB_analysis(z,t):
     
     A_pre = (1/16) * s * (s-1) * mp.power(mp.pi(),-1*s/2) * mp.gamma(s/2)
     A_sum = 0.0
-    for n in range(1,N+1):
-        if t.real > 0: 
+    for n in range(1, N+1):
+        if t.real > 0:
             A_sum += mp.exp((t/16)*mp.power(mp.log((s+4)/(2*mp.pi()*n*n)),2)) / mp.power(n,s)
-        else: 
+        else:
             A_sum += 1/mp.power(n,s) 
     A = A_pre*A_sum
     
     B_pre = (1/16) * s * (s-1) * mp.power(mp.pi(),(s-1)/2) * mp.gamma((1-s)/2)
     B_sum = 0.0
-    for m in range(1,M+1):
-        if t.real > 0: 
+    for m in range(1, M+1):
+        if t.real > 0:
             B_sum += mp.exp((t/16)*mp.power(mp.log((5-s)/(2*mp.pi()*m*m)),2)) / mp.power(m,1-s)
-        else: 
+        else:
             B_sum += 1/mp.power(m,1-s) 
     B = B_pre * B_sum
     
@@ -275,8 +275,8 @@ def AB_analysis(z,t):
     ABB0 = AplusB/B0
     return (AplusB, B0, ABB0, abs(ABB0)) 
 
-def adjusted_AB_analysis(z,t):
-    z,t = mp.mpc(z),mp.mpc(t)
+def adjusted_AB_analysis(z, t):
+    z, t = mp.mpc(z), mp.mpc(t)
     s = (1 + 1j*z.real - z.imag)/2
     tau = mp.sqrt(s.imag/(2*mp.pi()))
     N = int(tau)
@@ -288,36 +288,36 @@ def adjusted_AB_analysis(z,t):
     
     A_pre = initial_term * mp.power(mp.pi(),-1*s/2) * mp.exp((s_like1-0.5) * mp.log(s_like1)-s_like1)
     A_sum = 0.0
-    for n in range(1,N+1):
-        if t.real > 0: 
+    for n in range(1, N+1):
+        if t.real > 0:
             A_sum += mp.exp((t/16) * mp.power(mp.log(s_like1/(mp.pi()*n*n)),2)) / mp.power(n,s)
-        else: 
+        else:
             A_sum += 1/mp.power(n,s) 
     A = A_pre * A_sum
     
     B_pre = initial_term * mp.power(mp.pi(),(s-1)/2) * mp.exp((s_like2-0.5) * mp.log(s_like2)-s_like2)
     B_sum = 0.0
     ddxB_sum = 0.0
-    for m in range(1,M+1):
-        if t.real > 0: 
+    for m in range(1, M+1):
+        if t.real > 0:
             mth_term = mp.exp((t/16) * mp.power(mp.log(s_like2/(mp.pi()*m*m)),2)) / mp.power(m,1-s) 
-        else: 
+        else:
             mth_term = 1/mp.power(m,1-s)
         B_sum += mth_term
-        ddxB_sum += mth_term * mp.log(m) 
+        ddxB_sum += mth_term * mp.log(m)
     B = B_pre * B_sum
-    B_common_term = mp.exp((t/16) * mp.power(mp.log(s_like2/mp.pi()),2))    
+    B_common_term = mp.exp((t/16) * mp.power(mp.log(s_like2/mp.pi()),2))
     B0 = B_pre * B_common_term
     ddxBB0 = 0.5j * ddxB_sum / B_common_term
     
     AplusB = A + B
     ABB0 = AplusB/B0
-    return (AplusB, B0, ABB0, abs(ABB0), ddxBB0, abs(ddxBB0)) 
+    return (AplusB, B0, ABB0, abs(ABB0), ddxBB0, abs(ddxBB0))
 
 def Ht_AFE_ADJ_AB(z,t):
     '''This uses the adjusted A'+B' estimate to compute H_t for moderate to large T, where the C term is small.
-    Also, there are some rearrangements to the A' and B' formulas to make them faster to compute''' 
-    z,t = mp.mpc(z),mp.mpc(t)
+    Also, there are some rearrangements to the A' and B' formulas to make them faster to compute'''
+    z, t = mp.mpc(z), mp.mpc(t)
     s = (1 + 1j*z.real - z.imag)/2
     tau = mp.sqrt(s.imag/(2*mp.pi()))
     N = int(tau)
@@ -328,19 +328,19 @@ def Ht_AFE_ADJ_AB(z,t):
     log_sp1 = mp.log(s_like1/mp.pi())
     log_sp2 = mp.log(s_like2/mp.pi())
     initial_term = (1/4) * mp.sqrt(2*mp.pi())
-    A0 = initial_term * mp.power(mp.pi(),-1*s/2) * mp.exp((s_like1-0.5)*mp.log(s_like1) - s_like1 + (t/16)*mp.power(log_sp1,2)) 
-    B0 = initial_term * mp.power(mp.pi(),(s-1)/2) * mp.exp((s_like2-0.5)*mp.log(s_like2) - s_like2 + (t/16)*mp.power(log_sp2,2))   
+    A0 = initial_term * mp.power(mp.pi(),-1*s/2) * mp.exp((s_like1-0.5)*mp.log(s_like1) - s_like1 + (t/16)*mp.power(log_sp1,2))
+    B0 = initial_term * mp.power(mp.pi(),(s-1)/2) * mp.exp((s_like2-0.5)*mp.log(s_like2) - s_like2 + (t/16)*mp.power(log_sp2,2))
     A_sum_exponent = s + (t/4) * log_sp1
     B_sum_exponent = 1 - s + (t/4) * log_sp2
     
     A_sum = 0.0
-    for n in range(1,N+1): 
+    for n in range(1, N+1):
         A_sum += mp.power(n,(t/4)*mp.log(n) - A_sum_exponent)
     A = A0*A_sum
     
     B_sum = 0.0
-    for m in range(1,M+1): 
-        B_sum += mp.power(m,(t/4)*mp.log(m) - B_sum_exponent) 
+    for m in range(1, M+1):
+        B_sum += mp.power(m,(t/4)*mp.log(m) - B_sum_exponent)
     B = B0*B_sum
     
     H = A + B
