6049c681b0a75bd272643dde95a23ba34f80b9aa
KM <35831522+km-git-acc@users.noreply.github.com>
2018-03-04 00:24:24 +0530

    separated calculations for E3

--- dbn_upper_bound/python/research/habb_bound.py
+++ dbn_upper_bound/python/research/habb_bound.py
@@ -11,21 +11,6 @@ def append_data(filename, rows):
         writer = csv.writer(csvfile, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
         writer.writerows(rows)
 
-def findsubsets(S,m):
-    return set(combinations(S, m))
-
-def powerset(iterable):
-    xs = list(iterable)
-    return list(chain.from_iterable(combinations(xs,n) for n in range(len(xs)+1)))
-
-def deltaN(n,N):
-    if n<=N: return 1.0
-    else: return 0.0
-
-def divdelta(n,div):
-    if n%div==0: return 1.0
-    else: return 0.0
-
 def alpha1(s):
     return 0.5/s + 1/(s - 1) + 0.5 * mp.log(s/(2 * mp.pi()))
 
@@ -35,8 +20,8 @@ def H01(s):
 def habbbound(N,y=0.4,t=0.4):
     xN = 4*mp.pi()*N*N - t*mp.pi()/4.0
     xNp1 = 4*mp.pi()*(N+1)*(N+1) - t*mp.pi()/4.0
-    T1 = xN/2
-    T2 = xNp1/2
+    T1 = xN/2.0
+    T2 = xNp1/2.0
     Tdash1 = T1 + t*mp.pi()/8.0
     Tdash2 = T2 + t*mp.pi()/8.0
     sNminus = 0.5*(1-y + 0.5j*(xN+xNp1))
@@ -53,21 +38,52 @@ def habbbound(N,y=0.4,t=0.4):
         modterm_sNminus, modterm_sNplus = abs(alph_sNminus - mp.log(n)), abs(alph_sNplus - mp.log(n)) 
         cnminus = (t*t/4.0)*(modterm_sNminus**2 + 2*K*modterm_sNminus + K*K) + (1/3.0) + t
         cnplus = (t*t/4.0)*(modterm_sNplus**2 + 2*K*modterm_sNplus + K*K) + (1/3.0) + t
-        epsdash1+= cnminus*mp.exp(cnminus/(2*(T1-3.33)))/mp.power(nf,0.5*(1+y) + 0.5*t*(alph_sNminus.real - K) - 0.25*t*mp.log(n))
+        epsdash1+= cnminus*mp.exp(cnminus/(2*(T1-3.33)))/mp.power(nf,0.5*(1-y) + 0.5*t*(alph_sNminus.real - K) - 0.25*t*mp.log(n))
         epsdash2+= cnplus*mp.exp(cnplus/(2*(T1-3.33)))/mp.power(nf,0.5*(1+y) + 0.5*t*(alph_sNplus.real - K) - 0.25*t*mp.log(n))
     normalized_E1 = lambdafac*0.5*epsdash1/(T1-3.33)
     normalized_E2 = 0.5*epsdash2/(T1-3.33)
-    
-    E3_C0 = (1/8.0)*mp.sqrt(mp.pi()) * mp.exp(-1*(t/64.0)*(mp.pi()**2)) * mp.power(Tdash2, 1.5) * mp.exp(-1 * mp.pi() * T/4.0)
-    E3_vwf_bound = mp.exp(0.181/(Tdash1 - 3.33))*(1+5.15/(a0-1.25))
+    return [N, normalized_E1, normalized_E2]
+
+'''for N in range(1,501):
+    print(habbbound(N))'''
+
+def normalized_E3_xbound(x,y=0.4,t=0.4):
+    T = x/2.0
+    Tdash = T + t*mp.pi()/8.0
+    a0 = mp.sqrt(Tdash/(2*mp.pi()))
+            
+    E3_C0 = (1/8.0)*mp.sqrt(mp.pi()) * mp.exp(-1*(t/64.0)*(mp.pi()**2)) * mp.power(Tdash, 1.5) * mp.exp(-1 * mp.pi() * T/4.0)
+    E3_vwf_bound = mp.exp(0.181/(Tdash - 3.33))*(1+5.15/(a0-1.25))
     E3 = E3_C0*E3_vwf_bound
-    s2 = 0.5*(1+y) + 1j*T1
+    s2 = 0.5*(1+y) + 1j*T
     alph2 = alpha1(s2).conjugate()
     B0_expo = (t/4.0)*alph2*alph2
     H01_est2 = H01(s2).conjugate()
     B0_eff = (1/8.0)*mp.exp(B0_expo) * H01_est2
-    normalized_E3 = E3/B0_eff
+    normalized_E3 = abs(E3/B0_eff)
+    return [x, normalized_E3]
 
-    normalized_E = normalized_E1+normalized_E2+normalized_E3
-    return [N, normalized_E]
-    
+'''import time
+s=time.time()
+mp.pretty=True
+y=0.4
+t=0.4
+xstart = 1.0
+xend = xstart + 6*(10**5)
+mesh_size = 0.1
+csvfile="normalized_e3_data.csv"
+evaldata=[]
+i=0
+x=xstart
+while x < xend: 
+    zrow = [t,y]+normalized_E3_xbound(x)
+    evaldata.append(zrow)
+    #print(zrow)
+    if i % 5000 == 0: append_data(csvfile, evaldata); print(i,x); evaldata = []
+    x += mesh_size
+    i += 1
+
+e=time.time()
+e-s'''
+
+
